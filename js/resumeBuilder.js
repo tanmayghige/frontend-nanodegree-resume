fba5b2e7afa9b7aee9ca55b82ea9826ad30301cb
Tanmay Ghige <thghige@gmail.com>
2016-12-06 22:06:05 +0530

    Added Name and Role to Header

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,6 +1,12 @@
 //$("#main").append("Tanmay Ghige");
 
-var awesomeThoughts= "I am Tanmay and I am AWESOME!";
-console.log(awesomeThoughts);
-var funThoughts= awesomeThoughts.replace("AWESOME","FUN");
-$("#main").append(funThoughts);
+//var awesomeThoughts = "I am Tanmay and I am AWESOME!";
+//console.log(awesomeThoughts);
+//var funThoughts = awesomeThoughts.replace("AWESOME","FUN");
+//$("#main").append(funThoughts);
+
+var formattedName = HTMLheaderName.replace("%data%","Tanmay Ghige");
+var formattedRole = HTMLheaderRole.replace("%data%","Web Developer");
+
+$("#header").prepend(formattedRole);
+$("#header").prepend(formattedName);
