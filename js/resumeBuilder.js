d99fb80ad12219e86e8b0b150d3e5665bb6cd951
Tanmay Ghige <thghige@gmail.com>
2016-12-06 21:57:40 +0530

    Used string.replace() to replace awesome by fun

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,4 +1,6 @@
-$("#main").append("Tanmay Ghige");
+//$("#main").append("Tanmay Ghige");
 
 var awesomeThoughts= "I am Tanmay and I am AWESOME!";
 console.log(awesomeThoughts);
+var funThoughts= awesomeThoughts.replace("AWESOME","FUN");
+$("#main").append(funThoughts);
