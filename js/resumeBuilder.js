14ec39995f84edcffaaa428ee12d67a0589cfc36
Tanmay Ghige <thghige@gmail.com>
2016-12-10 16:48:00 +0530

    Added Object "bio"

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -5,6 +5,20 @@
 //var funThoughts = awesomeThoughts.replace("AWESOME","FUN");
 //$("#main").append(funThoughts);
 
+var bio = {
+    name : "Tanmay Ghige",
+    role : "Web Developer",
+    contacts : {
+        mobile : 1234567890,
+        email : "thghige@gmail.com",
+        github :"tanmayghige",
+        location :"Mumbai"
+    },
+    welcomeMessage : "Welcome to my Resume",
+    skills : ["awesomeness","delivering things","cryogenic sleep","saving the universe"],
+    bioPic : "images/fry.jpg"
+}
+
 var formattedName = HTMLheaderName.replace("%data%","Tanmay Ghige");
 var formattedRole = HTMLheaderRole.replace("%data%","Web Developer");
 
