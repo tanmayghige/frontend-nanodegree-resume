341d029d30dc6bae66abe0d310df769a6cc8f08a
Tanmay Ghige <thghige@gmail.com>
2016-12-17 17:30:44 +0530

    Added Education

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -71,7 +71,70 @@ $(".work-entry:last").append(formattedDates);
 $(".work-entry:last").append(formattedLocation);
 $(".work-entry:last").append(formattedDescription);
 
+var education = {
+    schools : [
+        {
+            name: "Mauris cursus ullamcorper",
+            degree: "BE",
+            years: "June 2012-may 2016",
+            city: "Mumbai, India",
+            major: "Electrical"
+        },
+        {
+            name: "Aliquam condimentum",
+            degree: "HSC",
+            years: "July 2010-May 2012",
+            city: "Mumbai, India",
+            major: "Science"
+        }
+    ],
+    onlineCourses :{
+        title: "Front-End Nanodegree",
+        school: "Udacity",
+        dates: 2016-17,
+        url: "https://www.udacity.com/"
+    }
+};
 
+$("#education").append(HTMLschoolStart);
+var formattedschoolName = HTMLschoolName.replace("%data%",education.schools[0].name);
+var formattedschoolDegree = HTMLschoolDegree.replace("%data%",education.schools[0].degree);
+$(".education-entry:last").append(formattedschoolName + formattedschoolDegree);
+
+var formattedschoolDates = HTMLschoolDates.replace("%data%",education.schools[0].years);
+$(".education-entry:last").append(formattedschoolDates);
+
+var formattedschoolLocation = HTMLschoolLocation.replace("%data%",education.schools[0].city);
+$(".education-entry:last").append(formattedschoolLocation);
+
+var formattedschoolMajor = HTMLschoolMajor.replace("%data%", education.schools[0].major);
+$(".education-entry:last").append(formattedschoolMajor);
+
+
+var formattedschoolName = HTMLschoolName.replace("%data%",education.schools[1].name);
+var formattedschoolDegree = HTMLschoolDegree.replace("%data%",education.schools[1].degree);
+$(".education-entry:last").append(formattedschoolName + formattedschoolDegree);
+
+var formattedschoolDates = HTMLschoolDates.replace("%data%",education.schools[1].years);
+$(".education-entry:last").append(formattedschoolDates);
+
+var formattedschoolLocation = HTMLschoolLocation.replace("%data%",education.schools[1].city);
+$(".education-entry:last").append(formattedschoolLocation);
+
+var formattedschoolMajor = HTMLschoolMajor.replace("%data%", education.schools[1].major);
+$(".education-entry:last").append(formattedschoolMajor);
+
+var formattedonlineTitle = HTMLonlineTitle.replace("%data%",education.onlineCourses.title);
+$(".education-entry:last").append(formattedonlineTitle);
+
+var formattedonlineSchool = HTMLonlineSchool.replace("%data%",education.onlineCourses.school);
+$(".education-entry:last").append(formattedonlineSchool);
+
+var formattedonlineDates = HTMLonlineDates.replace("%data%",education.onlineCourses.dates);
+$(".education-entry:last").append(formattedonlineDates);
+
+var formattedonlineURL = HTMLonlineURL.replace("%data%",education.onlineCourses.url);
+$(".education-entry:last").append(formattedonlineURL);
 
 
 
